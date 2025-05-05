e1a39ddf32f7705445b036e0784205cf49b7bd61
VictoriaCarvalho <vix219@icloud.com>
2025-05-05 10:26:15 -0500

    fruit types nav link

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -8,6 +8,7 @@ function NavBar() {
         <div className="nav-title">
       <ul className="nav ul">
         <li className="nav li"><Link to="/">Home</Link></li>
+        <li className="nav li">< Link to="/fruit-type">Fruit Types</Link></li>
         <li className="nav li"><Link to="/map">Map</Link></li>
         <li className="nav li"><Link to="/login">Login</Link></li>
         <li className="nav li"><Link to="/portal">User Portal</Link></li>
--- client/src/router.js
+++ client/src/router.js
@@ -6,6 +6,8 @@ import HomePage from "./components/HomePage";
 import UserLogin from "./components/UserLogin";
 import UserPortal from "./components/UserPortal";
 import MapComponent from "./components/MapComponent.jsx";
+// import TreeListComponent from "./components/TreeListComponent.jsx";
+import FruitType from "./components/FruitType.jsx";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +18,7 @@ const router = createBrowserRouter([
       { path: 'map', element: <MapComponent /> },
       { path: 'login', element: <UserLogin /> },
       { path: 'portal', element: <UserPortal /> },
+      { path: 'fruit-type', element: <FruitType /> },
     ],
   }
 ]);
